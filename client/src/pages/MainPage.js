eea2aa3a59c943950931bc4d3a8ba45192a4bfcd
CharithaMadhumal <charithamadhumal174@gmail.com>
2024-07-05 23:04:44 +0530

    client and server side
    completed

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,4 +1,6 @@
-import React , {useState} from 'react';
+import React , {useEffect, useState} from 'react';
+import axios from 'axios';
+
 
 
 export default function MainPage() {
@@ -7,19 +9,52 @@ export default function MainPage() {
     const [sourceCurrency, setSourceCurrency] = useState("");
     const [targetCurrency, setTargetCurrency] = useState("");
     const [amountInSourceCurrency, setAmountInSourceCurrency] = useState(0);
-    const [amountInTargetCurrency, setamountInTargetCurrency] = useState(0);
+    const [amountInTargetCurrency, setAmountInTargetCurrency] = useState(0);
+    const [currencyNames , setCurrencyNames] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     //handleSubmit method 
 
-    const handleSubmit = (e) =>{
+    const handleSubmit = async(e) =>{
         e.preventDefault();
-        console.log(date,setSourceCurrency,targetCurrency,amountInSourceCurrency);
+      
+       try{
+        const response = await axios.get("http://localhost:5000/convert",{ params: {
+            date,
+            sourceCurrency,
+            targetCurrency,
+            amountInSourceCurrency,
+        },
+
+        });
+
+        setAmountInTargetCurrency(response.data);
+        setLoading(false);
+        
+
+       }catch(err){
+           console.error(err);
+       }
+       
+
     }
+
+    useEffect(()=>{
+        const getCurrencyNames = async ()=>{
+            try{
+                const response = await axios.get("http://localhost:5000/getAllCurrencies");
+                setCurrencyNames(response.data);
+            }catch(err){
+                console.error(err);
+            }
+        }
+        getCurrencyNames();
+    },[])
 
   return (
     <div>
      
-     <h1 className='lg:mx-32 text-5xl font-bold text-green-500'>Convert Your Currencies Today</h1>
+     <h1 className='lg:mx-32 text-5xl font-bold text-yellow-300'>Convert Your Currencies Today</h1>
 
      <p className='lg:mx-32 opacity-35 py-6'>Welcome to "Convert Your Currencies Today"! This application allows you to easily convert Currencies based on the latest 
         exchange rates. Whether you're planning a trip, managing your finances, or simply curious about the value of your money
@@ -43,6 +78,11 @@ export default function MainPage() {
                     
                     name={sourceCurrency} id={sourceCurrency} value={sourceCurrency}>
                         <option value="">Select source currency</option>
+                        {Object.keys(currencyNames).map((currency)=>(
+                            <option className='p-1' key={currency} value={currency}>
+                                {currencyNames[currency]}
+                            </option>
+                            ))}
 
                     </select>
                 </div>
@@ -54,6 +94,11 @@ export default function MainPage() {
                     
                     name={targetCurrency} id={targetCurrency} value={targetCurrency}>
                         <option value="">Select target currency</option>
+                        {Object.keys(currencyNames).map((currency)=>(
+                            <option className='p-1' key={currency} value={currency}>
+                                {currencyNames[currency]}
+                            </option>
+                            ))}
 
                     </select>
                 </div>
@@ -61,14 +106,21 @@ export default function MainPage() {
                 <div className='mb-4'>
                     <label htmlFor={amountInSourceCurrency} class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Amount in source currency</label>
 
-                    <input onChange={(e) => setAmountInSourceCurrency(e.target.value)} type="number" id={amountInSourceCurrency} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Amount in source currency" required />
+                    <input onChange={(e) => setAmountInSourceCurrency(e.target.value)} type="number" id={amountInSourceCurrency} name={amountInSourceCurrency} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Amount in source currency" required />
                 </div>
 
-                <button  className='bg-green-700 hover:bg-green-500 text-white font-medium py-2 px-4 rounded-md'>Get the target Currency</button>
+                <button className='bg-green-700 hover:bg-green-500 text-white font-medium py-2 px-4 rounded-md'>Get the target Currency</button>
 
             </form>
         </section>
      </div>
+     {!loading ?  <section className='lg:mx-60 text-xl mt-5 '>
+
+       {amountInSourceCurrency} {currencyNames[sourceCurrency]} is equals to {" "}
+       <span className='text-yellow-300 font-bold'> {amountInTargetCurrency}</span> in {currencyNames[targetCurrency]};
+       </section> : null}
+    
+    
     </div>
   )
 }
--- server/src/index.js
+++ server/src/index.js
@@ -1,2 +1,67 @@
-console.log('hello world');
-console.log('hello world');
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+
+//middle wears
+
+app.use(express.json());
+app.use(cors());
+
+//All currencies
+
+app.get("/getAllCurrencies",async (req,res)=>{
+
+    const namesURL = "https://openexchangerates.org/api/currencies.json?app_id=76cd5c071a224d0788ce527cbd0e45df";
+
+   
+
+    try{ 
+
+        const namesResponce = await axios.get(namesURL);
+        const nameData = namesResponce.data;
+    
+        return res.json(nameData);
+
+    }catch(err){
+        console.error(err);
+    }
+});
+
+// get the target amount
+
+app.get("/convert", async(req,res)=>{
+    const {date, sourceCurrency, targetCurrency, amountInSourceCurrency} = req.query;
+
+    try{
+        const dataUrl = `https://openexchangerates.org/api/historical/${date}.json?app_id=76cd5c071a224d0788ce527cbd0e45df`;
+
+        const dataResponce = await axios.get(dataUrl);
+        const rates = dataResponce.data.rates;
+
+        
+
+        //rates 
+        const sourceRate = rates[sourceCurrency];
+        const targetRate = rates[targetCurrency];
+
+        //final target amount
+
+        const targetAmount = (targetRate / sourceRate) * amountInSourceCurrency;
+
+        return res.json(targetAmount.toFixed(2));
+
+    }catch(err){
+        console.error(err);
+    }
+
+})
+
+
+//listen to a port
+
+app.listen(5000, () => {
+    console.log("server is running on port 5000");
+});
+
